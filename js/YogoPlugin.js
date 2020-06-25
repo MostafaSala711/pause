7e8282bc42c830a85b5b75d65c093cd6d879a02e
unknown <mosala711@gmail.com>
2020-06-25 23:47:24 +0200

    Pause

---
+++ js/YogoPlugin.js
@@ -0,0 +1,24 @@
+/*global console, $, alert, prompt, plusplus*/
+
+
+$(document).ready(function () {
+    
+    "use strict";
+    
+    // Trigger Nice Scroll
+    $("html").niceScroll({
+        
+        cursorcolor: '#7b1798',
+        cursorwidth: '10px',
+        cursorborder: '1px solid #fff'
+        
+    });
+
+
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+
+
+   
+    
+});
